c840eb8e3bf0a98c9c456e3fd54a3174ca6a0a34
thanhcong <thanhcong.mssl@gmail.com>
2016-06-05 16:08:15 +0700

    Add "initial data" actions

--- src/modules/initial-data/action-types.js
+++ src/modules/initial-data/action-types.js
@@ -1,1 +1,2 @@
 export const CLEAR = 'initial-data/CLEAR';
+export const SET = 'initial-data/SET';
--- src/modules/initial-data/actions.js
+++ src/modules/initial-data/actions.js
@@ -1,8 +1,15 @@
 import * as actionTypes from './action-types';
 
-export const clear = (parameter) => {
+export const clearInitialData = (parameter) => {
   return {
     type: actionTypes.CLEAR,
     parameter
+  }
+};
+
+export const set = (parameter) => {
+  return {
+    type: actionTypes.SET,
+    ...parameter
   }
 };
