8bd18bfa265ddfb750179dc26e6eba9e87cdb5d2
Thanh Cong Nguyen <thanhcong.mssl@gmail.com>
2016-06-14 13:12:40 +0700

    Fix pagination size

--- src/routes/home/components/food-list.js
+++ src/routes/home/components/food-list.js
@@ -4,13 +4,17 @@ import React from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 const FoodList = ({data}) => {
+  const options = {
+    paginationSize: 3
+  };
+  
   return (
     <div className="FoodList">
-      <BootstrapTable data={data} striped={true} hover={true} pagination>
+      <BootstrapTable data={data} striped={true} hover={true} pagination options={options}>
           <TableHeaderColumn dataField="name" isKey={true} filter={ { type: 'TextFilter', delay: 300, placeholder: 'Search' } } dataAlign="center"></TableHeaderColumn>
           <TableHeaderColumn dataField="carb" dataSort={true} width="70">Carb</TableHeaderColumn>
           <TableHeaderColumn dataField="fat" dataSort={true} width="70">Fat</TableHeaderColumn>
-          <TableHeaderColumn dataField="protein" dataSort={true} sortFunc={ sortByName } width="100">Protein</TableHeaderColumn>
+          <TableHeaderColumn dataField="protein" dataSort={true} sortFunc={ sortByName } width="90">Protein</TableHeaderColumn>
           <TableHeaderColumn dataField="calo" dataSort={true} sortFunc={ sortByName } width="70">Calo</TableHeaderColumn>
       </BootstrapTable>
     </div>
