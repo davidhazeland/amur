53d2413fdee35dbc91feac91ea56a037eb88be63
Thanh Cong Nguyen <thanhcong.mssl@gmail.com>
2016-07-05 22:19:33 +0700

    Add try, catch to food util

--- src/utils/food.js
+++ src/utils/food.js
@@ -1,12 +1,21 @@
 const math = require('mathjs');
 
 export function resolve(data) {
-  const splits = data.split(' ');
-  const quantity = math.unit(splits.shift()).toNumber('gram');
-  const name = splits.join(' ');
+  try {
+    const splits = data.split(' ');
+    const quantity = math.unit(splits.shift()).toNumber('gram');
+    const name = splits.join(' ');
 
-  return {
-    quantity,
-    name
+    return {
+      quantity,
+      name
+    }
+  }
+  catch (e) {
+    console.log(e); /* eslint no-console: 0 */
+    return {
+      quantity: 0,
+      name: ''
+    }
   }
 }
